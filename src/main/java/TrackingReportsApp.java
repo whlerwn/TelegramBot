79d0bd003d06d53ec6733321be3c249bf98a30c0
Valeria Bozhko <whlerwn@yandex.ru>
2022-06-29 17:09:20 +0300

    add bot info

---
+++ src/main/java/TrackingReportsApp.java
@@ -0,0 +1,15 @@
+import bot.TrackingReportsBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class TrackingReportsApp {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new TrackingReportsBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/bot/TrackingReportsBot.java
@@ -0,0 +1,34 @@
+package bot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class TrackingReportsBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return "TrackingReportsBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5459953599:AAGzJmz5IZEaNcTAADcSxY3fhDy0PQwk74c";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
+
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
